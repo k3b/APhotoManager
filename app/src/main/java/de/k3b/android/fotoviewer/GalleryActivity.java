801abac7fa9687fcc9dd4b0eb9a7a2c6025dc3b0
k3b <klaus3b-github@yahoo.de>
2015-06-02 23:20:02 +0200

    Refactored from GalleryActivity to GalleryArrayFragment

--- app/src/main/java/de/k3b/android/fotoviewer/GalleryActivity.java
+++ app/src/main/java/de/k3b/android/fotoviewer/GalleryActivity.java
@@ -1,137 +1,24 @@
 package de.k3b.android.fotoviewer;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.app.LoaderManager;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
 
-public class GalleryActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
-    private static String TAG="CursorLoader";
-
-    private GridView galleryView;
-    private GalleryArrayAdapter galleryAdapter;
-    // private GalleryCursorAdapter galleryAdapter;
-    LoaderManager loadermanager;
-    CursorLoader cursorLoader;
-
+public class GalleryActivity extends Activity implements GalleryArrayFragment.OnGalleryInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gallery_activity);
-
-        galleryView = (GridView) findViewById(R.id.gridView);
-        galleryAdapter = new GalleryArrayAdapter(this, R.layout.gallery_grid_item, getData());
-        // galleryAdapter = new GalleryCursorAdapter(this, null);
-
-        // loadermanager=getLoaderManager();
-        // loadermanager=getLoaderManager();
-
-        galleryView.setAdapter(galleryAdapter);
-
-        galleryView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
-
-                //Create intent
-                Intent intent = new Intent(GalleryActivity.this, ImageViewActivity.class);
-                intent.putExtra("title", item.getTitle());
-                intent.putExtra("image", item.getImage());
-
-                //Start details activity
-                startActivity(intent);
-            }
-        });
-        
-        /**
-         * This initializes the loader and makes it active. If the loader
-         * specified by the ID already exists, the last created loader is reused.
-         * If the loader specified by the ID does not exist, initLoader() triggers
-         * the LoaderManager.LoaderCallbacks method onCreateLoader().
-         * This is where you implement the code to instantiate and return a new loader.
-         * Use restartLoader() instead of this, to discard the old data and restart the Loader.
-         * Hence, here the given LoaderManager.LoaderCallbacks implementation are associated with the loader.
-         */
-        // loadermanager.initLoader(1, null, this);
+        setContentView(R.layout.activity_gallery); // .gallery_activity);
     }
 
-    /**
-     * This creates and return a new Loader (CursorLoader or custom Loader) for the given ID. 
-     * This method returns the Loader that is created, but you don't need to capture a reference to it. 
-     */
-    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+    @Override
+    public void onGalleryClick(ImageItem item) {
+        //Create intent
+        Intent intent = new Intent(this, ImageViewActivity.class);
+        intent.putExtra("title", item.getTitle());
+        intent.putExtra("image", item.getImage());
 
-    //    String[] projection = { DatabaseHandler.UserTable.id, DatabaseHandler.UserTable.name };
-
-/**
- * This requires the URI of the Content Provider
- * projection is the list of columns of the database to return. Null will return all the columns
- * selection is the filter which declares which rows to return. Null will return all the rows for the given URI.
- * selectionArgs:  You may include ?s in the selection, which will be replaced
- * by the values from selectionArgs, in the order that they appear in the selection. 
- * The values will be bound as Strings.
- * sortOrder determines the order of rows. Passing null will use the default sort order, which may be unordered.
- * To back a ListView with a Cursor, the cursor must contain a column named _ID.
- */
-
-        // cursorLoader = new CursorLoader(this, DatabaseAccessUtility.CONTENT_URI, projection, null, null, null);
-        return cursorLoader;
-
-    }
-
-    /**
-     * Called when a previously created loader has finished its load. This assigns the new Cursor but does not close the previous one. 
-     * This allows the system to keep track of the Cursor and manage it for us, optimizing where appropriate. This method is guaranteed
-     * to be called prior to the release of the last data that was supplied for this loader. At this point you should remove all use of 
-     * the old data (since it will be released soon), but should not
-     * do your own release of the data since its loader owns it and will take care of that.
-     * The framework would take of closing of old cursor once we return.
-     */
-
-    public void onLoadFinished(Loader<Cursor> loader,Cursor cursor) {
-        if(galleryAdapter!=null && cursor!=null)
-           // galleryAdapter.swapCursor(cursor)
-           ; //swap the new cursor in.
-        else
-            Log.v(TAG,"OnLoadFinished: galleryAdapter is null");
-    }
-
-    /**
-     * This method is triggered when the loader is being reset and the loader  data is no longer available. 
-     * This is called when the last Cursor provided to onLoadFinished() above
-     * is about to be closed. We need to make sure we are no longer using it.
-     */
-
-    public void onLoaderReset(Loader<Cursor> arg0) {
-        if(galleryAdapter!=null)
-            // galleryAdapter.swapCursor(null)
-            ;
-        else
-            Log.v(TAG, "OnLoadFinished: galleryAdapter is null");
-    }
-
-    /**
-     * Prepare some dummy data for gridview
-     */
-    private ArrayList<ImageItem> getData() {
-        final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            imageItems.add(new ImageItem(bitmap, "Image#" + i));
-        }
-        return imageItems;
+        //Start details activity
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/de/k3b/android/fotoviewer/GalleryArrayFragment.java
@@ -0,0 +1,121 @@
+package de.k3b.android.fotoviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GalleryArrayFragment.OnGalleryInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link GalleryArrayFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryArrayFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private GridView galleryView;
+    private ListAdapter galleryAdapter;
+
+    private OnGalleryInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GalleryArrayFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GalleryArrayFragment newInstance(String param1, String param2) {
+        GalleryArrayFragment fragment = new GalleryArrayFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public GalleryArrayFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View result = inflater.inflate(R.layout.fragment_gallery, container, false);
+        galleryView = (GridView) result.findViewById(R.id.gridView);
+        galleryAdapter = new GalleryArrayAdapter(this.getActivity(), R.layout.gallery_grid_item, StaticDemoData.getData(this.getActivity()));
+
+        galleryView.setAdapter(galleryAdapter);
+
+        galleryView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+
+                if (mListener != null) {
+                    mListener.onGalleryClick(item);
+                }
+            }
+        });
+        return result;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnGalleryInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnGalleryInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnGalleryInteractionListener {
+        // TODO: Update argument type and name
+        public void onGalleryClick(ImageItem imageItem);
+    }
+
+}
