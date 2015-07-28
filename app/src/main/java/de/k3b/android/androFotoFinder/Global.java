99fb4fc73c04b7566389beda40048553d38cb55f
k3b <klaus3b-github@yahoo.de>
2015-07-28 23:23:23 +0200

    FDroid build with reduced debug output

--- app/src/main/java/de/k3b/android/androFotoFinder/Global.java
+++ app/src/main/java/de/k3b/android/androFotoFinder/Global.java
@@ -35,7 +35,7 @@ public class Global {
      */
     public static boolean debugEnabled = false;
     public static boolean debugEnabledViewItem = false;
-    public static boolean debugEnabledSql = true;
+    public static boolean debugEnabledSql = false;
 
     public static void debugMemory(String modul, String message) {
         if (false && Global.debugEnabled) {
