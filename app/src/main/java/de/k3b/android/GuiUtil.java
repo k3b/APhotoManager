00ae50b2bcacbcece91a9951ab8a8e3d540f41b6
k3b <klaus3b-github@yahoo.de>
2015-07-28 23:06:30 +0200

    added images for wiki

---
+++ app/src/main/java/de/k3b/android/GuiUtil.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 k3b
+ * 
+ * This file is part of de.k3b.android.toGoZip (https://github.com/k3b/ToGoZip/) .
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ */
+package de.k3b.android;
+
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+/**
+ * gui utils
+ */
+public class GuiUtil {
+    public static String getAppVersionName(final Context context) {
+        try {
+
+            final String versionName = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0).versionName;
+            return versionName;
+        } catch (final NameNotFoundException e) {
+        }
+        return null;
+    }
+
+}
